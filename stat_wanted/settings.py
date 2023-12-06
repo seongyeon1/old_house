29997ade119dda32d0a41024934e7b47276b95ab
seongyeon1 <83098550+seongyeon1@users.noreply.github.com>
2023-12-06 15:29:05 +0900

    config

--- stat_wanted/settings.py
+++ stat_wanted/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vxq4t)k+k^$2zkdngg)&1c5_0o6#vb_52)jj#+-f=88c$()qc7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
